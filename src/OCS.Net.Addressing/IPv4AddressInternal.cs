a7da062f27cdb415cabb85150365c992a83c3e06
oxffaa <akulyakov.artem@gmail.com>
2020-01-04 04:58:07 +0300

    specified internal ipv4 structure size explicitly

--- src/OCS.Net.Addressing/IPv4AddressInternal.cs
+++ src/OCS.Net.Addressing/IPv4AddressInternal.cs
@@ -5,7 +5,7 @@ using System.Runtime.InteropServices;
 
 namespace OCS.Net.Addressing
 {
-    [StructLayout(LayoutKind.Explicit)]
+    [StructLayout(LayoutKind.Explicit, Size = sizeof(UInt32))]
     internal struct IPv4AddressInternal
     {
         [FieldOffset(0)] public UInt32 Address;
@@ -26,7 +26,7 @@ namespace OCS.Net.Addressing
                     0 => Segment1,
                     1 => Segment2,
                     2 => Segment3,
-                    _ => Segment4
+                    _ => Segment4 // idx = 3
                 };
             }
             set
@@ -38,7 +38,7 @@ namespace OCS.Net.Addressing
                     case 0: Segment1 = value; break;
                     case 1: Segment2 = value; break;
                     case 2: Segment3 = value; break;
-                    default: Segment4 = value; break;
+                    default: Segment4 = value; break; // idx = 3
                 }
             }
         }

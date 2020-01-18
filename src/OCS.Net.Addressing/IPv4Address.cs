5d420e142f1da3fc8f9795463d63cf86d8f4c996
oxffaa <akulyakov.artem@gmail.com>
2020-01-18 21:34:00 +0700

    refactored ip v4 address source files and structure

--- src/OCS.Net.Addressing/IPv4Address.cs
+++ src/OCS.Net.Addressing/IPv4Address.cs
@@ -1,10 +1,11 @@
 using System;
+using System.Diagnostics.CodeAnalysis;
 using System.Runtime.CompilerServices;
 using OCS.Net.Addressing.Internal;
 
 namespace OCS.Net.Addressing
 {
-    public partial struct IPv4Address
+    public partial struct IPv4Address: IEquatable<IPv4Address>
     {
         public static readonly IPv4Address Empty = new IPv4Address();
         
@@ -71,5 +72,61 @@ namespace OCS.Net.Addressing
             
             return address;
         }
+        
+        #region Equatable
+        
+        [MethodImpl(MethodImplOptions.AggressiveInlining)]
+        public bool Equals(IPv4Address other) => this.value.Address == other.value.Address;
+
+        public override bool Equals(object obj)
+        {
+            if (ReferenceEquals(null, obj)) return false;
+            return obj is IPv4Address other && Equals(other);
+        }
+
+        [MethodImpl(MethodImplOptions.AggressiveInlining)]
+        public static bool operator ==(IPv4Address lo, IPv4Address ro) => lo.Equals(ro);
+
+        [MethodImpl(MethodImplOptions.AggressiveInlining)]
+        public static bool operator !=(IPv4Address lo, IPv4Address ro) => !(lo == ro);
+        
+        #endregion
+        
+        #region Parsing
+        
+        public static bool TryParse(string ip, out IPv4Address result)
+        {
+            if (ParserHelper.TryParseIPv4(
+                    ip.AsSpan(), // ip for parsing
+                        
+                    0, // start parsing position
+                    out var endPosition, // position of end parsing, out
+                        
+                    out var internalResult // result
+                ) 
+                && endPosition == ip.Length) // all string was parsed check
+            {
+                result = new IPv4Address(internalResult);
+                return true;
+            }
+            
+            result = Empty;
+            return false;
+        }
+
+        public static IPv4Address Parse(string ip)
+        {
+            if (!TryParse(ip, out var result))
+                ThrowArgumentException();
+            
+            return result;
+        }
+
+        [DoesNotReturn]
+        [MethodImpl(MethodImplOptions.AggressiveInlining)]
+        private static void ThrowArgumentException() =>
+            throw new ArgumentException("Expected valid ip v4 address");
+        
+        #endregion
     }
 }
